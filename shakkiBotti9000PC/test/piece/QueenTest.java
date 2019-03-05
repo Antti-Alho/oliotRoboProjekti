dff18e83de5bf72241432d5236d953cd3fdfbcaa
Antti Alho <antti.s.alho@gmail.com>
2019-03-05 12:23:36 +0200

    create rest of piece test files

---
+++ shakkiBotti9000PC/test/piece/QueenTest.java
@@ -0,0 +1,14 @@
+package piece;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class QueenTest {
+
+	@Test
+	void test() {
+		fail("Not yet implemented");
+	}
+
+}
