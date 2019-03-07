f93cf71aee6a4bb35317bf8fdf59d361ee22fb98
Pasi S <pasi.saarela90@gmail.com>
2019-03-07 15:17:51 +0200

    Tehty Pawntest

--- shakkiBotti9000PC/src/pieceTests/PawnTest.java
+++ shakkiBotti9000PC/src/pieceTests/PawnTest.java
@@ -3,23 +3,20 @@ package pieceTests;
 import static org.junit.jupiter.api.Assertions.*;
 
 import java.util.ArrayList;
-import java.util.Arrays;
-
+import java.util.Collections;
 import org.junit.jupiter.api.Test;
-
 import piece.Pawn;
 import shakkiBotti9000PC.Board;
 import shakkiBotti9000PC.Move;
+import shakkiBotti9000PC.MoveComparator;
 import shakkiBotti9000PC.Position;
-import org.junit.jupiter.api.AfterAll;
-import org.junit.jupiter.api.AfterEach;
-import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.Disabled;
+
 
 class PawnTest {
 
 	private Pawn pawn;
+	private Pawn pawnWhite;
 	private Board board;
 	private Position[][] positions;
 	
@@ -28,29 +25,76 @@ class PawnTest {
         System.out.println("--------------------");
         this.board = new Board();
         this.pawn = (Pawn) board.getPositions()[1][4].getPiece();
+        this.pawnWhite = (Pawn) board.getPositions()[6][4].getPiece();
     }
 	
     @Test
+    public void testGetMovesFirstMoveBlack() {
+        ArrayList<Move> movesExpected = new ArrayList<Move>();
+        movesExpected.add(new Move(pawn, board.getPositions()[2][4]));
+        movesExpected.add(new Move(pawn, board.getPositions()[3][4]));
+        
+        ArrayList<Move> movesReal = pawn.getMoves(board);
+        Collections.sort(movesReal, new MoveComparator());
+        
+        assertEquals(movesExpected, movesReal, "List of Black Pawn's start moves");
+    }
+    
+    @Test
+    public void testGetMovesNotFirstMoveBlack() {
+    	board.move(new Move(pawn, board.getPositions()[2][4]));
+    	positions = board.getPositions(); 
+    	
+    	ArrayList<Move> movesExpected = new ArrayList<Move>();
+        movesExpected.add(new Move(pawn, board.getPositions()[3][4]));
+        
+        ArrayList<Move> movesReal = pawn.getMoves(board);
+        Collections.sort(movesReal, new MoveComparator());
+        
+        assertEquals(movesExpected, movesReal, "List of Black Pawn's moves when pawn has already moved once");
+    }
+    
+    @Test
+    public void testGetMovesEatBlack() {
+    	
+    	
+    	positions = board.getPositions(); 
+    	board.move(new Move(pawn, board.getPositions()[5][4]));
+    	
+    	ArrayList<Move> movesExpected = new ArrayList<Move>();
+    	movesExpected.add(new Move(pawn, new Position(2, 4)));
+    	movesExpected.add(new Move(pawn, new Position(3, 4)));
+    	
+    	ArrayList<Move> movesReal = pawn.getMoves(board);
+        Collections.sort(movesReal, new MoveComparator());
+    	
+        assertEquals(movesExpected, movesReal, "list of black pawn's eating movements");
+    }
+    
+    @Test
     public void testGetMovesFirstMoveWhite() {
-        ArrayList<Move> moves = new ArrayList<Move>();
-        moves.add(new Move(pawn, board.getPositions()[2][4]));
-        moves.add(new Move(pawn, board.getPositions()[3][4]));
-        assertEquals(moves, pawn.getMoves(board), "List of Pawn start moves test");
+        ArrayList<Move> movesExpected = new ArrayList<Move>();
+        movesExpected.add(new Move(pawnWhite, board.getPositions()[5][4]));
+        movesExpected.add(new Move(pawnWhite, board.getPositions()[4][4]));
+        
+        ArrayList<Move> movesReal = pawnWhite.getMoves(board);
+        Collections.sort(movesReal, new MoveComparator());
+        
+        assertEquals(movesExpected, movesReal, "List of white pawn's start moves");
     }
     
     @Test
     public void testGetMovesNotFirstMoveWhite() {
-    	board.move(new Move(pawn, board.getPositions()[2][4]));
+    	board.move(new Move(pawnWhite, board.getPositions()[5][4]));
     	positions = board.getPositions(); 
-    	for (int i = 0; i < positions.length; i++) {
-			for (int j = 0; j < positions.length; j++) {
-				System.out.print(positions[i][j].getPieceString() + " ");
-			}
-			System.out.println("  "+(i+1));
-		}
-    	ArrayList<Move> moves = new ArrayList<Move>();
-        moves.add(new Move(pawn, board.getPositions()[3][4]));
-        assertEquals(moves, pawn.getMoves(board), "List of Pawn moves when pawn has allready moved onece");
+    	
+    	ArrayList<Move> movesExpected = new ArrayList<Move>();
+        movesExpected.add(new Move(pawnWhite, board.getPositions()[4][4]));
+        
+        ArrayList<Move> movesReal = pawnWhite.getMoves(board);
+        Collections.sort(movesReal, new MoveComparator());
+        
+        assertEquals(movesExpected, movesReal, "List of white pawn's moves when pawn has already moved once");
     }
     
     @Test
@@ -58,39 +102,16 @@ class PawnTest {
     	
     	
     	positions = board.getPositions(); 
-    	for (int i = 0; i < positions.length; i++) {
-			for (int j = 0; j < positions.length; j++) {
-				System.out.print(positions[i][j].getPieceString() + " ");
-			}
-			System.out.println("  "+(i+1));
-		}
+    	board.move(new Move(pawnWhite, board.getPositions()[2][1]));
     	
+    	ArrayList<Move> movesExpected = new ArrayList<Move>();
+    	movesExpected.add(new Move(pawnWhite, new Position(1, 0)));
+    	movesExpected.add(new Move(pawnWhite, new Position(1, 2)));
     	
-    	board.move(new Move(board.getPositions()[6][3].getPiece(), board.getPositions()[2][3]));
-    	board.move(new Move(board.getPositions()[6][5].getPiece(), board.getPositions()[2][5]));
-    	ArrayList<Move> moves = new ArrayList<Move>();
-    	moves.add(new Move(pawn, new Position(2, 3)));
-    	moves.add(new Move(pawn, new Position(2, 5)));
-    	moves.add(new Move(pawn, new Position(2, 4)));
-    	moves.add(new Move(pawn, new Position(3, 4)));
+    	ArrayList<Move> movesReal = pawnWhite.getMoves(board);
+        Collections.sort(movesReal, new MoveComparator());
     	
-    	
-    	positions = board.getPositions(); 
-    	for (int i = 0; i < positions.length; i++) {
-			for (int j = 0; j < positions.length; j++) {
-				System.out.print(positions[i][j].getPieceString() + " ");
-			}
-			System.out.println("  "+(i+1));
-		}
-    	
-    	
-        assertEquals(moves, pawn.getMoves(board), "list of pawn moves when pawn can eat");
-    }
-    
-    @Disabled
-    @Test
-    public void testGetMovesEnPessantWhite() {
-    	// 
+        assertEquals(movesExpected, movesReal, "list of white pawn's eating movements");
     }
 
 }
