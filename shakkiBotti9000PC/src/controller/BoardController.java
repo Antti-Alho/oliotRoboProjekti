4e5ff0d4a0164eb30e2371b66af2f3229ac021b3
Heidi Joensuu <cedi@hotmail.fi>
2019-03-12 13:12:28 +0200

    Camera changed

--- shakkiBotti9000PC/src/controller/BoardController.java
+++ shakkiBotti9000PC/src/controller/BoardController.java
@@ -8,10 +8,12 @@ import java.util.Scanner;
 
 import piece.King;
 import shakkiBotti9000PC.Board;
+import shakkiBotti9000PC.Camera;
 import shakkiBotti9000PC.ChessAI;
 import shakkiBotti9000PC.Move;
 
 public class BoardController {
+	static Scanner sc = new Scanner(System.in);
 	
 	public static void main(String[] args) {
 		Board board = new Board();
@@ -23,47 +25,37 @@ public class BoardController {
 		Socket socket = null;
 		boolean vuoro = false;
 		boolean peli = true;
-		
+		Camera cam = new Camera(board);	
+		String answer=null;
 /*
-		try {
-			socket = new Socket("10.0.1.1", 1111);
-			DataInputStream in = new DataInputStream(socket.getInputStream());
-			DataOutputStream out = new DataOutputStream(socket.getOutputStream());
-			
 				while (board.getPieces().contains(white) && board.getPieces().contains(black)) {
 					vuoro = in.readBoolean();
 					if (peli==true) {
 						System.out.println("-----------------------");
-						System.out.println(" 0 1 2 3 4 5 6 7");
+						System.out.println("A B C D E F G H");
 						for (int i = 0; i < 8; i++) {
 							
 							for (int j = 0; j < 8; j++) {
 								System.out.print(" "+board.getPositions()[i][j].getPieceString());
 							}
-							System.out.println(" "+i);
+							System.out.println(" "+(i+1));
 						}
 						System.out.println("");
-						System.out.println("-----------------------");
-						System.out.print("Anna mistä x:");
-						int x = Integer.parseInt(s.nextLine());
-						System.out.print("Anna mistä y:");
-						int y = Integer.parseInt(s.nextLine());
-						System.out.print("Anna mihin x:");
-						int x2 = Integer.parseInt(s.nextLine());
-						System.out.print("Anna mihin y:");
-						int y2 = Integer.parseInt(s.nextLine());
+					
+						cam.takePicture();
+						cam.getEnemymove();
 						
-						board.move(new Move(board.getPositions()[x][y].getPiece(), board.getPositions()[x2][y2]));
+//						board.move(new Move(board.pieceAt(x, y),x2,y2,board.pieceAt(x2, y2)));
 						Move m = ai.calculateBestMove(depth);
 						board.move(m);
 						//fromX, toX, fromY, toY, target
-						out.writeInt(m.getP().getPos().getX());
+						out.writeInt(m.getP().getX());
 						out.flush();
-						out.writeInt(m.getP().getPos().getY());
+						out.writeInt(m.getP().getY());
 						out.flush();
-						out.writeInt(m.getNewPos().getX());
+						out.writeInt(m.getNewX());
 						out.flush();
-						out.writeInt(m.getNewPos().getY());
+						out.writeInt(m.getNewY());
 						out.flush();
 						if (m.getTarget() == null) {
 							out.writeInt(-1);
@@ -72,45 +64,37 @@ public class BoardController {
 							out.writeInt(1);
 							out.flush();
 						}
-						
 					}
 					vuoro = false;
 				}
 		} catch (IOException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
-	*/
-
-		while (board.getPieces().contains(white) && board.getPieces().contains(black)) {
-			System.out.println("-----------------------");
-			System.out.println(" 0 1 2 3 4 5 6 7");
-			for (int i = 0; i < 8; i++) {
-				
-				for (int j = 0; j < 8; j++) {
-					System.out.print(" "+board.getPositions()[i][j].getPieceString());
-				}
-				System.out.println(" "+i);
-			}
-			
-			System.out.println("");
-			System.out.println("-----------------------");
-			System.out.print("Anna mistä x:");
-			int x = Integer.parseInt(s.nextLine());
-			System.out.print("Anna mistä y:");
-			int y = Integer.parseInt(s.nextLine());
-			System.out.print("Anna mihin x:");
-			int x2 = Integer.parseInt(s.nextLine());
-			System.out.print("Anna mihin y:");
-			int y2 = Integer.parseInt(s.nextLine());
-			
-			board.move(new Move(board.getPositions()[x][y].getPiece(), x2,y2, board.pieceAt(x2, y2)));
-			System.out.println("lasketaan");
-			Move m = ai.calculateBestMove(depth);
-			board.move(m);
-			System.out.println("Laskettu");
-
 		}
-		
+*/
+		while (board.getPieces().contains(white) && board.getPieces().contains(black)) {
+
+			while (answer != "x") {
+				
+				
+				cam.takePicture();
+				cam.getEnemymove();
+				System.out.println("lasketaan");
+				Move m = ai.calculateBestMove(depth);
+				board.move(m);
+				System.out.println("Laskettu");
+				System.out.println("-----------------------");
+				System.out.println(" 0 1 2 3 4 5 6 7");
+				for (int i = 0; i < 8; i++) {
+					
+					for (int j = 0; j < 8; j++) {
+						System.out.print(" "+board.getPositions()[i][j].getPieceString());
+					}
+					System.out.println(" "+i);
+				}
+				System.out.println("********************* \n\nAgain");
+				answer = sc.next();
+			}
+		}
 	}
-	
 }
